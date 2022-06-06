4264c4c12c4324a5187e8a91ea21e5879f38d44f
Aleksei Chepurko <achepurko@devexperts.com>
2022-06-06 16:59:48 +0300

    provided all DI dependencies to the AppContainer

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import { routes } from '@routes/routes';
+import { HttpClientService } from '@services/httpClient.service';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
+import { AppContainer } from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const App = AppContainer({
+	restApi: new HttpClientService('https://remarkable-bombolone-51a3d9.netlify.app'),
+	imageApi: new HttpClientService('https://api.themoviedb.org/3'),
+	youtubeApi: new HttpClientService('https://www.googleapis.com/youtube/v3'),
+	imageApiUrl: 'https://image.tmdb.org/t/p/w500/',
+	routing: routes,
+	youtubeEmbedApi: '//www.youtube.com/embed',
+	imageApiKey: process.env.imageApiKey || '',
+	youtubeApiKey: process.env.youtubeApiKey || '',
+});
+
+root.render(App);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
