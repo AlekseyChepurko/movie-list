0537c679cea8f62949e9685f8e7aaddf8d0d79dd
Aleksei Chepurko <achepurko@devexperts.com>
2022-06-06 15:12:51 +0300

    lint+husky+TS+craco configs

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const srcDirPath = path.resolve(__dirname, './src');
+module.exports = {
+	webpack: {
+		alias: {
+			'@ui': path.join(srcDirPath, 'ui'),
+			'@features': path.join(srcDirPath, 'features'),
+			'@pages': path.join(srcDirPath, 'pages'),
+			'@services': path.join(srcDirPath, 'services'),
+			'@domain': path.join(srcDirPath, 'domain'),
+			'@controllers': path.join(srcDirPath, 'controllers'),
+			'@codecs': path.join(srcDirPath, 'codecs'),
+			'@utils': path.join(srcDirPath, 'utils'),
+		},
+	},
+};
